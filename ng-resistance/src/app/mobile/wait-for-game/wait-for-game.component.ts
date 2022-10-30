2f56d451397d901fa72dd0ceaaf85967fe58278b
Alex Reed <areed829@gmail.com>
2022-10-29 19:27:28 -1000

    refactor: Remove debug code

--- ng-resistance/src/app/mobile/wait-for-game/wait-for-game.component.ts
+++ ng-resistance/src/app/mobile/wait-for-game/wait-for-game.component.ts
@@ -1,14 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { switchMap, timer } from 'rxjs';
 import { MobileService } from '../mobile.service';
 
 @Component({
   templateUrl: 'wait-for-game.component.html',
 })
 export class WaitForGameComponent implements OnInit {
-  isFirst$ = timer(1000).pipe(
-    switchMap((_) => this.mobileService.isFirstPlayer()),
-  );
+  isFirst$ = this.mobileService.isFirstPlayer();
 
   constructor(private mobileService: MobileService) {}
 
